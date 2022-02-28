4b770015b0c981081c13ac9441c19b53b56f4de8
David <davihup@gmail.com>
2022-02-28 19:05:23 +0100

    Test functions

---
+++ sources/test.py
@@ -0,0 +1,36 @@
+#Test functions
+from numpy import pi, arccos, arcsin, sin, cos, sqrt, linspace, zeros, array
+from matplotlib import pyplot as plt 
+
+def square_function():
+    t = linspace(0,100,100)
+    y = zeros(len(t))
+    for i in range(0,int(len(t)/2)):
+        if i%2 == 1:
+            y[2*i] = 1
+            y[2*i+1] = 1
+        else:
+            y[2*i] = -1 
+            y[2*i+1] = -1 
+
+    return [t,y] 
+
+def triangle_function():
+    t = linspace(0,100,100)
+    y = zeros(len(t))
+    for i in range(0,int(len(t)/2)):
+        y[2*i] = 0
+        y[2*i+1] = 1  
+
+    return[t,y] 
+
+if __name__ == "__main__":
+    [t, X] = triangle_function()
+
+     #Plots
+    plt.figure()
+    plt.plot(t,X)
+    plt.xlabel('t')
+    plt.ylabel('X (t)')
+    plt.title('Test function') 
+    plt.show()
