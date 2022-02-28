76a3cdf1accf1f32ad9709ee7e2ae429a666d822
David <davihup@gmail.com>
2022-02-28 19:47:30 +0100

    bispectral transform

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 import os
 import sys
 
+from matplotlib import pyplot as plt
+
 try:
     sys.path.insert(1, '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/'))+'/sources')
 except:
     sys.path.insert(1, '/'.join(os.path.dirname(os.path.abspath(__file__)).split('\\'))+'/sources')
 
-import test
+import test_function
 import bispectrum
+
+#Square function transformation 
+
+[t, X] = test_function.square_function()
+
+plt.figure()
+plt.plot(t,X)
+plt.xlabel('t')
+plt.ylabel('X (t)')
+plt.title('Square function') 
+
+bs = bispectrum.bispectral_transform(t, X)
+
+plt.figure()
+bs.plot_mag()
+# plt.show()
+
+#Triangle function transformation 
+
+[t, Y] = test_function.triangle_function()
+
+plt.figure()
+plt.plot(t,Y)
+plt.xlabel('t')
+plt.ylabel('Y (t)')
+plt.title('Triangle function') 
+
+bs = bispectrum.bispectral_transform(t, Y)
+
+plt.figure()
+bs.plot_mag()
+# plt.show()
+
+#Sum of square + triangle functions
+#  
+plt.figure()
+plt.plot(t,X+Y)
+plt.xlabel('t')
+plt.ylabel('X+Y (t)')
+plt.title('Square + Triangle function') 
+
+bs = bispectrum.bispectral_transform(t, X+Y)
+
+plt.figure()
+bs.plot_mag()
+plt.show()
+
 
--- sources/bispectrum.py
+++ sources/bispectrum.py
@@ -2,3 +2,19 @@ from numpy import array, linspace
 
 from stingray import lightcurve
 from stingray.bispectrum import Bispectrum
+
+def bispectral_transform(t, X):
+    """Computes the High Order Spectral Transform based on the
+    Fourier transform of third order auto-correlation function.
+
+    Intent(in): t (numpy.array), timestamps;
+                X (numpy.array), time series.
+
+    Returns: bs (object), bispectrum of the time series.
+    """
+
+    lc = lightcurve.Lightcurve(t,X, dt=t[1]-t[0])
+
+    bs = Bispectrum(lc, maxlag=10, window="uniform", scale="biased")
+
+    return bs
