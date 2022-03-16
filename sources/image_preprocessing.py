e7bd649367e5f67ee4f28a552167180103bc7b92
David <davihup@gmail.com>
2022-03-16 14:03:01 +0100

    CNN model

--- sources/image_preprocessing.py
+++ sources/image_preprocessing.py
@@ -14,32 +14,64 @@ except:
 import test_function
 import bispectrum
 
-t0 = time.time()
+def process_raw_dataset():
 
-[X, Y] = test_function.read_dataset('ElectricDevices/ElectricDevices_TEST.txt') 
+    t0 = time.time()
 
-t = linspace(0, 24*60, len(X[0,:]))
+    [X, Y] = test_function.read_dataset('ElectricDevices/ElectricDevices_TEST.txt') 
 
-X_test = [] 
+    t = linspace(0, 24*60, len(X[0,:]))
 
-for i in range(0, len(X[:,0])):
-    bs = bispectrum.bispectral_transform(t, X[i,:])
-    bs.bispec_mag = bs.bispec_mag/linalg.norm(bs.bispec_mag)
-    X_test.append(bs.bispec_mag)
+    X_test = [] 
 
-X_test = array(X_test)
-X_test = X_test.reshape(len(X[:,0]),21,21,1)
+    for i in range(0, len(X[:,0])):
+        bs = bispectrum.bispectral_transform(t, X[i,:])
+        bs.bispec_mag = bs.bispec_mag/linalg.norm(bs.bispec_mag)
+        X_test.append(bs.bispec_mag)
 
-Y_test = to_categorical(Y)
+    X_test = array(X_test)
+    X_test = X_test.reshape(len(X[:,0]),21,21,1)
 
-print(X_test.shape)
+    Y = Y -1.0
+    Y_test = to_categorical(Y)
 
-print(Y_test.shape)
+    print(X_test.shape)
 
-tf = time.time()
+    print(Y_test.shape)
 
-print('t=', tf-t0)
+    tf = time.time()
 
-# save('ElectricDevices/X_test.npy', X_test)
-# save('ElectricDevices/Y_test.npy', Y_test)
+    print('t=', tf-t0)
 
+    # save('ElectricDevices/X_test.npy', X_test)
+    # save('ElectricDevices/Y_test.npy', Y_test)
+
+def process_raw_data():
+
+    t0 = time.time()
+
+    tags = ['W_Air_cond','W_Computers','W_Audio_TV','W_Lights','W_Kitchen','W_Washing_m','W_Dish_w','W_Gas_boiler','W_Oven_vitro'] 
+
+    X_eval = [] 
+    for tag in tags:
+        [t, X] = test_function.read('data/Sanse/20220301.plt', tag) 
+
+        bs = bispectrum.bispectral_transform(t, X)
+        bs.bispec_mag = bs.bispec_mag/linalg.norm(bs.bispec_mag)
+        X_eval.append(bs.bispec_mag)
+
+    X_eval = array(X_eval)
+    X_eval = X_eval.reshape(len(tags),21,21,1)
+
+
+    print(X_eval.shape)
+
+    tf = time.time()
+
+    print('t=', tf-t0)
+
+    # save('ElectricDevices/X_eval.npy', X_eval)
+
+if __name__ == "__main__":
+    # process_raw_dataset()
+    # process_raw_data()
--- sources/neural_network.py
+++ sources/neural_network.py
@@ -3,33 +3,51 @@ from keras.layers import Conv2D, Dense, MaxPooling2D, Dropout, Flatten
 from keras.utils import to_categorical
 from keras.datasets import mnist
 
-def CNN_model():
+from numpy import load
+
+def CNN_model(input_shape, output_shape):
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=(28,28,1)))
-    model.add(MaxPooling2D(pool_size=(3,3)))
+    model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(3,3), padding='same'))
     model.add(Dropout(0.1))
     model.add(Conv2D(32, kernel_size=5, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(4,4)))
+    model.add(MaxPooling2D(pool_size=(4,4), padding='same'))
     model.add(Dropout(0.1))
     model.add(Flatten())
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(output_shape, activation='softmax'))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
 if __name__ == "__main__":
-    CNN = CNN_model()
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    CNN = CNN_model((21,21,1), 7)
+    # (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-    X_train = X_train.reshape(60000,28,28,1)
-    X_test = X_test.reshape(10000,28,28,1)
+    # X_train = X_train.reshape(60000,28,28,1)
+    # X_test = X_test.reshape(10000,28,28,1)
 
-    print(y_train[0] )
+    # print(X_train[0] )
 
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
+    # print(y_train[0] )
 
-    print(y_train[0] )
+    # y_train = to_categorical(y_train)
+    # y_test = to_categorical(y_test)
 
-    CNN.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=8)
+    # print(y_train[0] )
+
+    X_train = load('ElectricDevices/X_train.npy')
+    X_test = load('ElectricDevices/X_test.npy')
+    y_train = load('ElectricDevices/Y_train.npy')
+    y_test = load('ElectricDevices/Y_test.npy')
+    X_eval = load('ElectricDevices/X_eval.npy')
+
+    # print(X_train.shape)
+    # print(y_train.shape)
+    # print(X_test.shape)
+    # print(y_test.shape)
+
+    CNN.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
+
+    prediction = CNN.predict(X_eval)
+    print(prediction)
